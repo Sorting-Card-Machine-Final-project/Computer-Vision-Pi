70f9bb00b78794230a9c80c28998f6d0e5eeed2f
Yuval <yuvally@gmail.com>
2024-10-10 21:36:59 +0300

    Adding function to interpret the message and send it threw UART

--- UART-STM-Connection.py
+++ UART-STM-Connection.py
@@ -9,8 +9,19 @@ ser = serial.Serial(port, baudrate)
 def sendViaUART(tray, start, end, moreCards):
 	data = 0x00
 	
+	#tray is a number between 1 to 4 and has 2 bits
+	data = data | bin(tray - 1)
+	
+	if start > 0:
+		data = data | 0b100
+	if end > 0:
+		data = data | 0b1000
+	if moreCards > 0:
+		data = data | 0b10000
+		
+	
 	# add handle to convert inputs to bits
 	
-	ser.write(bytes([data])
+	ser.write(bin(data))
 	
 	ser.close() # check when do I need to do that
