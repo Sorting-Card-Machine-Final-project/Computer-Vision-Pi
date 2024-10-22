91547cff29a93c79f8241bd8e4b16c8d50fa1ae8
Yuval <yuval.halperin7@gmail.com>
2024-10-22 13:52:07 +0300

    change uart to class

--- UART-STM-Connection.py
+++ UART-STM-Connection.py
@@ -1,27 +1,54 @@
 import gpoizero as gz
 import serial
 
-port = "dev/ttyS0" # Supposed to be the right port for the pins
-boudrate = 9600
+# port = "dev/ttyS0" # Supposed to be the right port for the pins
+# boudrate = 9600
 
-ser = serial.Serial(port, baudrate)
+# ser = serial.Serial(port, baudrate)
 
-def sendViaUART(tray, start, end, moreCards):
-	data = 0x00
+# def sendViaUART(tray, start, end, moreCards):
+# 	data = 0x00
 	
-	#tray is a number between 1 to 4 and has 2 bits
-	data = data | bin(tray - 1)
+# 	#tray is a number between 1 to 4 and has 2 bits
+# 	data = data | bin(tray - 1)
 	
-	if start > 0:
-		data = data | 0b100
-	if end > 0:
-		data = data | 0b1000
-	if moreCards > 0:
-		data = data | 0b10000
+# 	if start > 0:
+# 		data = data | 0b100
+# 	if end > 0:
+# 		data = data | 0b1000
+# 	if moreCards > 0:
+# 		data = data | 0b10000
 		
 	
-	# add handle to convert inputs to bits
+# 	# add handle to convert inputs to bits
 	
-	ser.write(bin(data))
+# 	ser.write(bin(data))
 	
-	ser.close() # check when do I need to do that
+# 	ser.close() # check when do I need to do that
+
+
+class UARTSend:
+	def __init__(self):
+		port = "dev/ttyS0" # Supposed to be the right port for the pins
+		boudrate = 9600
+		self.ser = serial.Serial(port, boudrate)
+
+	def sendViaUART(self, tray, start, end, moreCards):
+		data = 0x00
+		
+		#tray is a number between 1 to 4 and has 2 bits
+		data = data | bin(tray - 1)
+		
+		if start > 0:
+			data = data | 0b100
+		if end > 0:
+			data = data | 0b1000
+		if moreCards > 0:
+			data = data | 0b10000
+			
+		
+		# add handle to convert inputs to bits
+		
+		self.ser.write(bin(data))
+		
+		self.ser.close() # check when do I need to do that
